b045b0a9b3611946a95e3b24eb44f269d7d06374
Lucas Mercier <pro@lucasmercier.com>
2022-11-13 22:38:30 +0100

    initial commit

---
+++ src/main/kotlin/com/lucasmercier/portfolio/controller/GitHubController.kt
@@ -0,0 +1,12 @@
+package com.lucasmercier.portfolio.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/github")
+class GitHubController {
+
+
+
+}
