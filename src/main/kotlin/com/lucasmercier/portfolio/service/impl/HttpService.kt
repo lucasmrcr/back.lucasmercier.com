dc5ef52228f6b55c92263808601ab8c0e9c3df87
lucasmercier <pro@lucasmercier.com>
2022-11-16 00:03:32 +0100

    fix(github): build object mapper using jacksonObjectMapper function

--- src/main/kotlin/com/lucasmercier/portfolio/service/impl/HttpService.kt
+++ src/main/kotlin/com/lucasmercier/portfolio/service/impl/HttpService.kt
@@ -1,15 +1,17 @@
 package com.lucasmercier.portfolio.service.impl
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.lucasmercier.portfolio.service.IHttpService
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
 
 @Service
 class HttpService : IHttpService {
     override fun <T> get(url: String, aClass: Class<T>): T {
-        val objectMapper = ObjectMapper()
+        val objectMapper = jacksonObjectMapper()
         val client = OkHttpClient.Builder().build()
         val request = Request.Builder()
             .url(url)
