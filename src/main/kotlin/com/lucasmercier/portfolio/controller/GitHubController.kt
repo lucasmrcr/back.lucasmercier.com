fcc123bdcd9fc5938fea46e1712127579e997074
lucasmercier <114911557+lucasmercierredlab@users.noreply.github.com>
2022-11-19 14:42:57 +0100

    fix(github): add cross origin

--- src/main/kotlin/com/lucasmercier/portfolio/controller/GitHubController.kt
+++ src/main/kotlin/com/lucasmercier/portfolio/controller/GitHubController.kt
@@ -4,12 +4,14 @@ import com.lucasmercier.portfolio.controller.adapter.RepositoryAdapter
 import com.lucasmercier.portfolio.controller.dto.ListRepositoryDTO
 import com.lucasmercier.portfolio.service.IGitHubService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/github")
+@CrossOrigin
 class GitHubController(
     private val githubService: IGitHubService,
 ) {
